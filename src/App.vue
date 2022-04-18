98882c5d40df20e36c04a518c05f082a139ec6b7
gotha liu <gotha331@gmail.com>
2022-04-18 10:47:16 +0800

    feat: Directive demo

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
   <A></A>
   <B></B>
 
+  <DragTab></DragTab>
+
+
   <button @click="flag = !flag">chnage flag</button>
   <div>{{ flag }}</div>
 </template>
@@ -14,6 +17,8 @@
 
 import A from './components/Mitt/A.vue'
 import B from './components/Mitt/B.vue'
+
+import DragTab from './components/DragTab/index.vue'
 
 let flag = ref<boolean>(false)
 
