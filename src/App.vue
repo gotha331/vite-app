9125712a17549f336983542f8970aa09fb46afb3
gotha liu <gotha331@gmail.com>
2022-04-18 10:59:09 +0800

    feat: hooks

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <!-- <Layout></Layout> -->
-
   <A></A>
   <B></B>
 
   <DragTab></DragTab>
 
-
   <button @click="flag = !flag">chnage flag</button>
   <div>{{ flag }}</div>
+
+  <div>
+    <img id="img" width="300" height="300" src="./assets/test.jpg">
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -17,10 +19,15 @@
 
 import A from './components/Mitt/A.vue'
 import B from './components/Mitt/B.vue'
-
 import DragTab from './components/DragTab/index.vue'
 
+import useBase64 from './hooks'
+
 let flag = ref<boolean>(false)
+
+useBase64({ el: "#img" }).then(res => {
+  console.log(res.baseUrl);
+})
 
 </script>
 
